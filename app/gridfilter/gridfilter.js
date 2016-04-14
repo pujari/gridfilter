85128ffbaac774f689eb1c91f803a06a3e191d67
Pujari, Ashish <Ashish.Pujari@iriworldwide.com>
2016-04-14 10:40:29 -0500

    added flags

--- app/gridfilter/gridfilter.js
+++ app/gridfilter/gridfilter.js
@@ -10,49 +10,54 @@ angular.module('myApp.gridfilter', ['ngRoute', 'angular-loading-bar'])
 }])
 
 .controller('GridFilterCtrl', ["$scope","$http", function($scope, $http){
-	$scope.MAX_POP = 1340000000;
-	$scope.minPop = 0;
-	$scope.maxPop = $scope.MAX_POP;
-
-	$scope.MAX_AREA = 17200000;
-	$scope.minArea = 0;
-	$scope.maxArea = $scope.MAX_AREA;
-
- 	$scope.labels = ["#",
-    	"Code",
-    	"Country",
-    	"Population",
-    	"Area (sq km)"];
-
-	$scope.slider1 = {
-	  min: 0,
-	  max: $scope.MAX_POP,
-	  options: {
-		floor: 0,
-		ceil: $scope.MAX_POP
-	  }
-	};
-
-	$scope.slider2 = {
-	  min: 0,
-	  max: $scope.MAX_AREA,
-	  options: {
-		floor: 0,
-		ceil: $scope.MAX_AREA
-	  }
-	};
-
 	//load countries data
 	$http.get('countries.json').success(function(response){
 		$scope.countries = response;
+
+		//population min, max values
+		$scope.minPop = Math.min.apply(Math,$scope.countries.map(function(item){return item.population;}));
+		$scope.maxPop = Math.max.apply(Math,$scope.countries.map(function(item){return item.population;}));
+		$scope.MIN_POP = $scope.minPop;
+		$scope.MAX_POP = $scope.maxPop;
+
+		//area min, max values
+		$scope.minArea = Math.min.apply(Math,$scope.countries.map(function(item){return item.areaInSqKm;}));
+		$scope.maxArea = Math.max.apply(Math,$scope.countries.map(function(item){return item.areaInSqKm;}));
+		$scope.MIN_AREA = $scope.minArea;
+		$scope.MAX_AREA = $scope.maxArea;
+
+		$scope.labels = ["#",
+			"Code",
+			"Country",
+			"Population",
+			"Area (sq km)",
+			"Flag"];
+
+		$scope.slider1 = {
+		  min: 0,
+		  max: $scope.MAX_POP,
+		  options: {
+			floor: 0,
+			ceil: $scope.MAX_POP
+		  }
+		};
+
+		$scope.slider2 = {
+		  min: 0,
+		  max: $scope.MAX_AREA,
+		  options: {
+			floor: 0,
+			ceil: $scope.MAX_AREA
+		  }
+		};
+
+		$scope.byRange = function (fieldName, minValue, maxValue) {
+		  if (minValue === undefined) minValue = 0;
+		  if (maxValue === undefined) maxValue = $scope.MAX_POP;
+
+		  return function predicateFunc(item) {
+			return minValue <= item[fieldName] && item[fieldName] <= maxValue;
+		  };
+		};
 	});
-
-	$scope.byRange = function (fieldName, minValue, maxValue) {
-	  if (minValue === undefined) minValue = 0;
-	  if (maxValue === undefined) maxValue = $scope.MAX_POP;
-
-	  return function predicateFunc(item) {
-		return minValue <= item[fieldName] && item[fieldName] <= maxValue;
-	  };
-	};
 }]);
