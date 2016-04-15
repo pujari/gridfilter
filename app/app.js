903a84e0f5396039b8698efec5a44611d3905d02
Pujari, Ashish <Ashish.Pujari@iriworldwide.com>
2016-04-15 08:33:32 -0500

    Fixed flags

--- app/app.js
+++ app/app.js
@@ -2,7 +2,19 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', ['ngRoute','angular-loading-bar','myApp.gridfilter', 'rzModule'])
+
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider
   .otherwise({redirectTo: '/gridfilter'});
-}]);
+}])
+
+.directive('fallbackSrc', function () {
+  var fallbackSrc = {
+    link: function postLink(scope, iElement, iAttrs) {
+      iElement.bind('error', function() {
+        angular.element(this).attr("src", iAttrs.fallbackSrc);
+      });
+    }
+   }
+   return fallbackSrc;
+});
